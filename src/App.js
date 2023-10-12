7bef34d7c7bbd778f143604bcdfed0b73f02d002
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-10-12 11:59:28 +0100

    Added eventHandlers to the undo buttons

--- src/App.js
+++ src/App.js
@@ -66,17 +66,25 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
   }
   function handleTripleInc() {
-    setLikes(likes + 1);
-    setLikes(likes + 1);
-    setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
   }
 
   function handleUndo(params) {
     setShowDetails(true);
     setLikes(0);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -98,7 +106,7 @@ function TabContent({ item }) {
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
